94c0177b822d4d18f9e876c083ddb2fffc251145
hjlarry <hjlarry@163.com>
2019-09-29 16:08:01 +0800

    sort out struct

--- basic/struct_test.go
+++ basic/struct_test.go
@@ -1,7 +1,7 @@
-package main
+package basic
 
 import (
-	"fmt"
+	"testing"
 	"unsafe"
 )
 
@@ -26,27 +26,27 @@ type employee struct {
 }
 
 // TestStruct ... 结构体
-func TestStruct() {
+func TestStruct(t *testing.T) {
 	tom := person{"Tom", 25}
-	fmt.Println(tom)
-	fmt.Println(tom.age)
+	t.Log(tom)
+	t.Log(tom.age)
 
 	mark := student{person: person{"Mark", 30}, special: "computer science"}
-	fmt.Println(mark)
-	fmt.Println(mark.name)
+	t.Log(mark)
+	t.Log(mark.name)
 	mark.name = "haha"
-	fmt.Println(mark.name)
+	t.Log(mark.name)
 	mark.person.age -= 2
-	fmt.Println(mark.age)
+	t.Log(mark.age)
 
 	mark.skills = append(mark.skills, "golang", "python")
-	fmt.Println(mark.skills)
+	t.Log(mark.skills)
 	mark.int = 3
-	fmt.Println(mark.int)
+	t.Log(mark.int)
 
 	larry := employee{person{"larry", 12}, "designer", 20}
-	fmt.Println(larry.age)
-	fmt.Println(larry.person.age)
+	t.Log(larry.age)
+	t.Log(larry.person.age)
 }
 
 // Circle ...
@@ -57,22 +57,22 @@ type Circle struct {
 }
 
 // TestStruct1 ... 结构体相互赋值本质上是浅拷贝，拷贝了其内部所有字段，而结构体指针的相互赋值仅拷贝了指针地址值
-func TestStruct1() {
+func TestStruct1(t *testing.T) {
 	var c1 = Circle{Radius: 50}
 	var c2 = c1
-	fmt.Printf("%+v\n", c1)
-	fmt.Printf("%+v\n", c2)
+	t.Log(c1)
+	t.Log(c2)
 	c1.Radius = 100
-	fmt.Printf("%+v\n", c1)
-	fmt.Printf("%+v\n", c2)
+	t.Log(c1)
+	t.Log(c2)
 
 	var c3 = &Circle{Radius: 50}
 	var c4 = c3
-	fmt.Printf("%+v\n", c3)
-	fmt.Printf("%+v\n", c4)
+	t.Log(c3)
+	t.Log(c4)
 	c3.Radius = 100
-	fmt.Printf("%+v\n", c3)
-	fmt.Printf("%+v\n", c4)
+	t.Log(c3)
+	t.Log(c4)
 }
 
 // ArrayStruct ...
@@ -86,8 +86,8 @@ type SliceStruct struct {
 }
 
 // TestStruct2 ... 数组占用的大小是数组容量，而切片只是头部容量
-func TestStruct2() {
+func TestStruct2(t *testing.T) {
 	var as = ArrayStruct{[...]int{0, 1, 2, 3, 4, 5, 6, 7, 8}}
 	var ss = SliceStruct{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8}}
-	fmt.Println(unsafe.Sizeof(as), unsafe.Sizeof(ss))
+	t.Log(unsafe.Sizeof(as), unsafe.Sizeof(ss))
 }
