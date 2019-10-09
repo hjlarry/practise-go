f2f51b626f99d52a4b84a8d88eeb12a146b2a7d2
hjlarry <hjlarry@163.com>
2019-10-09 15:30:43 +0800

    chan wait all task complete example

--- concurrent/task_complete_test.go
+++ concurrent/task_complete_test.go
@@ -24,9 +24,33 @@ func firstResponse() string {
 	return <-ch
 }
 
+// 当第一个任务完成时返回
 func TestFirstResponse(t *testing.T) {
 	t.Log("before goroutines:", runtime.NumGoroutine())
 	t.Log(firstResponse())
+	time.Sleep(time.Second)
+	t.Log("after goroutines:", runtime.NumGoroutine())
+}
+
+func allResponse() string {
+	ch := make(chan string, 4)
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			ch <- runTask(i)
+		}(i)
+	}
+
+	ret := ""
+	for j := 0; j < 10; j++ {
+		ret = ret + <-ch + "\n"
+	}
+	return ret
+}
+
+// 当所有任务完成时返回，除了使用csp的方式，也可以使用waitGroup来实现
+func TestAllResponse(t *testing.T) {
+	t.Log("before goroutines:", runtime.NumGoroutine())
+	t.Log(allResponse())
 	time.Sleep(time.Second)
 	t.Log("after goroutines:", runtime.NumGoroutine())
 }
