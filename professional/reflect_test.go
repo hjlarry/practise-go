f892d0fd4d3d7d08afbd9c50c2a4b8d10cdfd01b
hjlarry <hjlarry@163.com>
2019-10-24 15:00:02 +0800

    add more about reflect type

--- professional/reflect_test.go
+++ professional/reflect_test.go
@@ -1,16 +1,101 @@
 package professional
 
 import (
-	"github.com/pkg/errors"
+	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/pkg/errors"
 )
 
 func TestTypeAndValue(t *testing.T) {
 	var f uint64 = 99
-	t.Log(reflect.ValueOf(f))
-	t.Log(reflect.TypeOf(f))
+	t.Log(reflect.ValueOf(f), reflect.TypeOf(f))
 	t.Log(reflect.ValueOf(f).Type())
+
+	// Type表示其真实类型，Kind表示其底层类型
+	type X int
+	var a X = 100
+	r := reflect.TypeOf(a)
+	t.Log(r.Name(), r.Kind())
+
+	// 可直接构造一些基础的复合类型
+	b := reflect.ArrayOf(10, reflect.TypeOf(byte(0)))
+	c := reflect.MapOf(reflect.TypeOf(""), reflect.TypeOf(0))
+	t.Log(b)
+	t.Log(c)
+
+	// 传入对象应区分基类型和指针类型
+	y := 100
+	ty, tp := reflect.TypeOf(y), reflect.TypeOf(&y)
+	t.Log(ty, tp)
+	t.Log(ty.Kind(), tp.Kind())
+	t.Log(ty == tp.Elem())
+
+	// Elem返回指针、数组、切片、字典值、通道的基类型
+	t.Log(reflect.TypeOf(map[string]int{}).Elem())
+	t.Log(reflect.TypeOf([]int32{}).Elem())
+
+}
+
+/* 遍历结构体字段 */
+type user struct {
+	name string
+	age  int
+}
+type manager struct {
+	user
+	title string
+}
+
+func TestStructField(t *testing.T) {
+	var m manager
+	tt := reflect.TypeOf(&m)
+	if tt.Kind() == reflect.Ptr { // 遍历struct的字段，只能获取结构体指针的基类型
+		tt = tt.Elem()
+	}
+	for i := 0; i < tt.NumField(); i++ {
+		f := tt.Field(i)
+		fmt.Println(f.Name, f.Type, f.Offset)
+		if f.Anonymous { // 若是匿名字段结构
+			for x := 0; x < f.Type.NumField(); x++ {
+				af := f.Type.Field(x)
+				fmt.Println(af.Name, af.Type, af.Offset)
+			}
+		}
+	}
+
+	// 按名称查找，但不支持多级名称，如果同名遮蔽须通过匿名字段二次获取
+	name, _ := tt.FieldByName("name")
+	fmt.Println(name.Name, name.Type)
+	// 按多级索引查找
+	age := tt.FieldByIndex([]int{0, 1})
+	fmt.Println(age.Name, age.Type)
+}
+
+/* 遍历方法集，也区分指针类型或基类型 */
+type A int
+type B struct {
+	A
+}
+
+func (A) Av()  {}
+func (*A) Ap() {}
+func (B) Bv()  {}
+func (*B) Bp() {}
+
+func TestMethodSet(t *testing.T) {
+	var b B
+	tt := reflect.TypeOf(&b)
+	ss := tt.Elem()
+	fmt.Println(tt, ":")
+	for i := 0; i < tt.NumMethod(); i++ {
+		fmt.Println(" ", tt.Method(i))
+	}
+	fmt.Println(ss, ":")
+	for i := 0; i < ss.NumMethod(); i++ {
+		fmt.Println(" ", ss.Method(i))
+	}
 }
 
 func checkType(v interface{}) {
@@ -122,4 +207,23 @@ func TestFillNameAge(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log(c)
+}
+
+/* Implements、ConvertibleTo、AssignableTo使用*/
+type Y int
+
+func (Y) String() string {
+	return ""
+}
+func TestAssitMethod(t *testing.T) {
+	var a Y
+	tt := reflect.TypeOf(a)
+	// Implements不能直接使用类型作为参数
+	st := reflect.TypeOf((*fmt.Stringer)(nil)).Elem()
+	t.Log(tt.Implements(st))
+
+	it := reflect.TypeOf(0)
+	t.Log(tt.ConvertibleTo(it))
+
+	t.Log(tt.AssignableTo(st), tt.AssignableTo(it))
 }
