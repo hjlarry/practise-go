7878476fc6603b500afc089ee0428cf0c812c4c6
hjlarry <hjlarry@163.com>
2020-03-17 14:58:33 +0800

    sort out string

--- language_detail/data/str.go
+++ language_detail/data/str.go
@@ -2,24 +2,75 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"unsafe"
+	"strconv"
+	"strings"
 )
 
+// 一、字符串基础
 // 字符串的本质也是struct{ptr, len}
 // 但它和slice不同的是，会把字符串的结构体和它的底层字节序列看做一体的，底层序列单独拿出是没有意义的
-
-// 由于字符串是只读的，所以不允许去拿字节序列元素的指针
-// 子串和原串可以共享底层字节数组
-// func main() {
-// 	s := "hello, world"
-// 	sub := s[:6]
-// 	fmt.Printf("%#v \n", *(*reflect.StringHeader)(unsafe.Pointer(&s)))
-// 	fmt.Printf("%#v \n", *(*reflect.StringHeader)(unsafe.Pointer(&sub)))
-// }
+//func main() {
+//	//由于字符串是只读的，所以不允许去拿字节序列元素的指针
+//	//子串和原串可以共享底层字节数组
+//	ss := "hello, world"
+//	sub := ss[:6]
+//	fmt.Printf("%#v \n", *(*reflect.StringHeader)(unsafe.Pointer(&ss)))
+//	fmt.Printf("%#v \n", *(*reflect.StringHeader)(unsafe.Pointer(&sub)))
+//
+//	// 字符串的零值为空
+//	var a string
+//	fmt.Println("s" + a + "b")
+//
+//	// 字符串字面量允许十六进制、八进制和UTF编码格式
+//	var s = "嘻哈ch\x61\142\u0041"
+//	fmt.Println(s)
+//	// len返回底层字节数组的长度,RuneCountInString返回准确的unicode字符数量
+//	fmt.Println(len(s), utf8.RuneCountInString(s))
+//	// 截取并拼接一个不合法的字符串
+//	s = string(s[0:1] + s[3:4])
+//	fmt.Println(s, utf8.ValidString(s))
+//
+//	// 可使用`不做转义处理
+//	s = `line\r\n
+//      line2`
+//	fmt.Println(s)
+//	// 允许索引号访问字节数组（非字符串），但不能使用&s[3]访问其地址，也不能s[3]='2'进行赋值
+//	fmt.Println(s[3])
+//
+//	// 切片语法返回的子串，其内部依旧指向原字节数组
+//	s1 := s[:3]
+//	s2 := s[1:4]
+//	fmt.Println(s1, s2)
+//	fmt.Printf("%#v\n", (*reflect.StringHeader)(unsafe.Pointer(&s)))
+//	fmt.Printf("%#v\n", (*reflect.StringHeader)(unsafe.Pointer(&s1)))
+//
+//	// 两种遍历方式: byte和rune
+//	s = "哈a"
+//	for i := 0; i < len(s); i++ {
+//		fmt.Println(s[i])
+//	}
+//	for codepoint, runeValue := range s {
+//		fmt.Println(codepoint, int32(runeValue))
+//	}
+//
+//	//使用单引号的字面量，默认为rune
+//	r := '我'
+//	fmt.Printf("%T", r)
+//
+//	sr := "中华人民共和国"
+//	for _, c := range sr {
+//		fmt.Printf("%[1]c %[1]d", c)
+//	}
+//
+//	for i, w := 0, 0; i < len(sr); i += w {
+//		runeValue, width := utf8.DecodeRuneInString(sr[i:])
+//		fmt.Printf("%#U starts at byte position %d\n", runeValue, i)
+//		w = width
+//	}
+//}
 
 /* ------------------------------------------------------------------------------------- */
-
+// 二、传递字符串
 // go build -gcflags "-S" 2>a.txt str.go
 // 字符串传递时也只传其字节数组地址和长度
 // 0x001d 00029 (/root/.mac/gocode/str.go:22)	LEAQ	go.string."hello,world!"(SB), AX
@@ -39,6 +90,8 @@ import (
 // }
 
 /* ------------------------------------------------------------------------------------- */
+// 三、 字符串转换
+// 1. 转换行为发生复制
 // func main() {
 // 	s := "hello, world"
 
@@ -64,15 +117,65 @@ import (
 // }
 
 // 但是反过来转为slice时，少一个cap，必须构建一个头部
+//func main() {
+//	s := "hello, world!"
+//	sh := *(*reflect.StringHeader)(unsafe.Pointer(&s))
+//	bh := reflect.SliceHeader{
+//		Data: sh.Data,
+//		Len:  sh.Len,
+//		Cap:  sh.Len,
+//	}
+//	bs := *(*[]byte)(unsafe.Pointer(&bh))
+//	fmt.Printf("%#v \n", sh)
+//	fmt.Printf("%#v \n", *(*reflect.SliceHeader)(unsafe.Pointer(&bs)))
+//}
+
+// 2. 修改字符串
+//func pp(format string, ptr interface{}) {
+//	p := reflect.ValueOf(ptr).Pointer()
+//	h := (*uintptr)(unsafe.Pointer(p))
+//	fmt.Printf(format, *h)
+//}
+//
+//// 要修改字符串，可以将其转换为[]byte或[]rune，修改完后转回，但都会重新分配内存、复制数据
+//// 使用"非安全"方法可以改善
+//func main() {
+//	s := "hello, world!"
+//	pp("s: %x\n", &s)
+//
+//	bs := []byte(s)
+//	s2 := string(bs)
+//	pp("string to []byte,  bs: %x\n", &bs)
+//	pp("[]byte to string,  s2: %x\n", &s2)
+//
+//	rs := []rune(s)
+//	s3 := string(bs)
+//	pp("string to []rune,  bs: %x\n", &rs)
+//	pp("[]rune to string,  s2: %x\n", &s3)
+//
+//	bb := []byte("hello world!")
+//	ss := *(*string)(unsafe.Pointer(&bb))
+//	pp("bb: %x\n", &bb)
+//	pp("ss: %x\n", &ss)
+//
+//}
+
+// 3. 使用strings和strconv包
 func main() {
-	s := "hello, world!"
-	sh := *(*reflect.StringHeader)(unsafe.Pointer(&s))
-	bh := reflect.SliceHeader{
-		Data: sh.Data,
-		Len:  sh.Len,
-		Cap:  sh.Len,
-	}
-	bs := *(*[]byte)(unsafe.Pointer(&bh))
-	fmt.Printf("%#v \n", sh)
-	fmt.Printf("%#v \n", *(*reflect.SliceHeader)(unsafe.Pointer(&bs)))
+	s := "a,b,c,d"
+	list := strings.Split(s, ",")
+	fmt.Println(list)
+	s = strings.Join(list, "-")
+	fmt.Println(s)
+
+	i := 1
+	ss := strconv.Itoa(i)
+	fmt.Printf("%T", ss)
+	// 字符串转数字时会返回值+错误
+	b, _ := strconv.Atoi(ss)
+	fmt.Printf("%T", b)
+	c, err := strconv.Atoi("a")
+	fmt.Println(c)
+	fmt.Println(err)
 }
+
--- language_detail/data/str_test.go
+++ language_detail/data/str_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 	"unsafe"
 )
 
+/* safe和unsafe的方式字符串转换性能比较 */
 // go test -bench . -benchmem str_test.go
 // BenchmarkTestStr/safe-2         	205193720	         5.52 ns/op	       0 B/op	       0 allocs/op
 // BenchmarkTestStr/unsafe-2       	1000000000	         0.279 ns/op	       0 B/op	       0 allocs/op
@@ -21,4 +23,66 @@ func BenchmarkTestStr(b *testing.B) {
 			_ = *(*reflect.StringHeader)(unsafe.Pointer(&bs))
 		}
 	})
+}
+
+
+/* 直接拼接、使用join、使用bytes.buffer的性能比较 */
+func plus() string {
+	var s string
+	for i := 0; i < 1000; i++ {
+		s += "a"
+	}
+	return s
+}
+
+//BenchmarkPlus-4   	   10000	    106563 ns/op
+func BenchmarkPlus(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		plus()
+	}
+}
+
+func join(a []string, sep string) string {
+	n := len(sep) * (len(a) - 1)  // 统计分隔符长度
+	for i := 0; i < len(a); i++ { // 统计所有待拼接字符长度
+		n += len(a[i])
+	}
+	b := make([]byte, n) //一次分配所需长度的数组空间
+	bp := copy(b, a[0])
+	for _, s := range a[1:] {
+		bp += copy(b[bp:], sep)
+		bp += copy(b[bp:], s)
+	}
+	return string(b)
+}
+
+func testJoin() string {
+	s := make([]string, 1000) //准备足够内存，避免中途扩张
+	for i := 0; i < 1000; i++ {
+		s[i] = "a"
+	}
+	return join(s, "")
+}
+
+//BenchmarkJoin-4   	  200000	      9145 ns/op
+func BenchmarkJoin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		testJoin()
+	}
+}
+
+func buffer() string {
+	var b bytes.Buffer
+	b.Grow(1000) //准备足够内存，避免中途扩张
+	for i := 0; i < 1000; i++ {
+		b.WriteString("a")
+	}
+	return b.String()
+}
+
+//BenchmarkBuffer-4   	  200000	      6213 ns/op
+func BenchmarkBuffer(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		buffer()
+	}
 }
