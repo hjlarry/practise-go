cb2131a76921bb397cde41030ee8d37d3c664935
hjlarry <hjlarry@163.com>
2020-03-23 12:14:15 +0800

    sort out number

--- language_detail/data/number.go
+++ language_detail/data/number.go
@@ -1,66 +1,66 @@
-package basic
+package main
 
 import (
 	"errors"
-	"testing"
+	"fmt"
 )
 
-func TestVar(t *testing.T) {
+func main() {
 	// 有符号整数，可以表示正负
 	var a int8 = 1  // 1 字节
 	var b int16 = 2 // 2 字节
 	var c int32 = 3 // 4 字节
 	var d int64 = 4 // 8 字节
-	t.Log(a, b, c, d)
-	t.Logf("%T", &a)
-	t.Logf("%T", *&a)
+	fmt.Println(a, b, c, d)
+	fmt.Printf("%T", &a)
+	fmt.Printf("%T", *&a)
 	// 无符号整数，只能表示非负数
 	var ua uint8 = 1
 	var ub uint16 = 2
 	var uc uint32 = 3
 	var ud uint64 = 4
-	t.Log(ua, ub, uc, ud)
+	fmt.Println(ua, ub, uc, ud)
 
 	// int 类型，在32位机器上占4个字节，在64位机器上占8个字节
 	var e int = 5
 	var ue uint = 5
-	t.Log(e, ue)
+	fmt.Println(e, ue)
 
 	// bool 类型
 	var f bool = true
-	t.Log(f)
+	fmt.Println(f)
 
 	// 字节类型
 	var j byte = 'a'
-	t.Log(j)
+	fmt.Println(j)
 
 	// 字符串类型
 	var g string = "abcdefg"
-	t.Log(g)
+	fmt.Println(g)
 
 	// 浮点数
 	var h float32 = 3.14
 	var i float64 = 3.141592653
-	t.Log(h, i)
+	fmt.Println(h, i)
 
 	var aArray [9]int
-	t.Log(aArray)
+	fmt.Println(aArray)
 
 	var aa = [10]int{1, 3, 4}
 	var bb [10]int = [10]int{1, 3, 4}
 	cc := [10]int{1, 3, 4}
-	t.Log(aa)
-	t.Log(bb)
-	t.Log(cc)
+	fmt.Println(aa)
+	fmt.Println(bb)
+	fmt.Println(cc)
 
 	var seq [9]int
 	for i := 0; i < len(seq); i++ {
 		seq[i] = i * i
 	}
-	t.Log(seq)
+	fmt.Println(seq)
 
 	err := errors.New("emit macho dwarf: elf header corrupted")
 	if err != nil {
-		t.Log(err)
+		fmt.Println(err)
 	}
 }
