67838b0c4a7e363b9b97d21a692f39e5f562ee1b
hjlarry <hjlarry@163.com>
2020-04-13 10:21:38 +0800

    add comment to raft

--- small_programe/raft/raft.go
+++ small_programe/raft/raft.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 	"net/http"
 	"net/rpc"
-	"sync"
 	"time"
 )
 
@@ -24,17 +23,27 @@ const (
 )
 
 type Raft struct {
-	mu sync.Locker
-
-	me          int
-	role        Role
+	me          int  //当前节点id
+	role        Role // 当前角色
 	currentTerm int
-	voteFor     int
-	voteCount   int
-	nodes       map[int]*Node
+	voteFor     int           // 给谁投票，未投时为-1
+	voteCount   int           // 当前任期内获得的票数
+	nodes       map[int]*Node // 集群内除自己的其他节点map
 
-	toLeaderCh  chan bool
-	heartbeatCh chan bool
+	toLeaderCh  chan bool // 其中取出true，说明计算出自己是Leader
+	heartbeatCh chan bool // 其中取出true，说明收到leader发出的心跳
+
+	logs        []LogEntry
+	commitIndex int
+	lastApplied int
+	nextIndex   []int
+	matchIndex  []int
+}
+
+type LogEntry struct {
+	LogTerm    int
+	LogIndex   int
+	LogCommand interface{}
 }
 
 func (rf *Raft) String() string {
@@ -68,13 +77,16 @@ func (rf *Raft) start() {
 
 	go func() {
 		rand.Seed(time.Now().UnixNano())
+		// 不间断处理节点行为和RPC
 		for {
 			switch rf.role {
 			case Follower:
 				select {
+				// 心跳超时，follower变为Candidate
 				case <-time.After(time.Duration(rand.Intn(500-300)+300) * time.Millisecond):
 					fmt.Printf("follower %d timeout \n", rf.me)
 					rf.role = Candidate
+				//	成功接收到心跳
 				case <-rf.heartbeatCh:
 					fmt.Printf("follower %d received heartbeat \n", rf.me)
 				}
@@ -87,9 +99,11 @@ func (rf *Raft) start() {
 				go rf.broadcastVoteRequest()
 
 				select {
+				// 选举超时，candiate变为follower
 				case <-time.After(time.Duration(rand.Intn(500-300)+300) * time.Millisecond):
 					fmt.Printf("timeout, candiate %d become follower", rf.me)
 					rf.role = Follower
+				//	选举成功
 				case <-rf.toLeaderCh:
 					fmt.Printf("candiate %d become leader", rf.me)
 					rf.role = Leader
@@ -142,6 +156,7 @@ func (rf *Raft) sendRequestVote(nodeId int, args RequestVoteArgs, reply *Request
 		return
 	}
 
+	// 每次成功接收到投票时，计算有没有超过半数，超过则通知toLeaderCh
 	if reply.VoteGrant {
 		rf.voteCount += 1
 
@@ -152,6 +167,7 @@ func (rf *Raft) sendRequestVote(nodeId int, args RequestVoteArgs, reply *Request
 }
 
 func (rf *Raft) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error {
+	// 作为其他节点，收到一个投票请求，确认是否要投票
 	if args.Term < rf.currentTerm {
 		reply.Term = rf.currentTerm
 		reply.VoteGrant = false
@@ -203,9 +219,11 @@ func (rf *Raft) sendHeartBeat(nodeId int, args HeartbeatArgs, reply *HeartbeatRe
 
 	_ = client.Call("Raft.HeartBeat", args, reply)
 
+	// 收到心跳回复后，发现自己的term小于其他节点，说明已经过期，自己变为Follower，重新选举
 	if reply.Term > rf.currentTerm {
 		rf.currentTerm = reply.Term
 		rf.voteFor = -1
+		rf.voteCount = 0
 		rf.role = Follower
 	}
 }
